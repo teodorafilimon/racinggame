88afc1386dd9c051945dd8e9a0f78c8f82642e6e
Teodora Filimon <teodora.filimon@yahoo.ro>
2019-11-13 11:17:49 -0800

    studied classes and variables

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,8 +6,60 @@ package org.fasttrackit;
  */
 public class App 
 {
+
+    // one line comment
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the racing game" );
+        //instance variables
+
+       Car carReference = new Car();
+       carReference.name = "Dacia";
+       carReference.color = "red";
+       carReference.mileage = 9.8;
+       carReference.fuelLevel = 60;
+       carReference.maxSpeed = 200;
+       carReference.running = false;
+       carReference.doorCount= 2;
+
+       Car car2 = new Car();
+       car2.name = "BMW";
+       car2.mileage = 14;
+//concanetaton
+        System.out.println(carReference);
+        System.out.println("First car name: " + carReference.name);
+        System.out.println(carReference.color);
+        System.out.println(carReference.mileage);
+        System.out.println(carReference.fuelLevel);
+        System.out.println(carReference.maxSpeed);
+        System.out.println(carReference.traveledDistance);
+        System.out.println(carReference.running);
+
+
+        System.out.println("Second car name: " + car2.name);
+        System.out.println(car2.color);
+        System.out.println(car2.mileage);
+        System.out.println(car2.fuelLevel);
+        System.out.println(car2.maxSpeed);
+        System.out.println(car2.traveledDistance);
+        System.out.println(car2.running);
+
+        car2.name = "VW";
+        System.out.println(car2.name);
+
+        Car car3 = car2;
+        car3.name = "Audi";
+
+        System.out.println("Car2 name: " + car2.name);
+        System.out.println("Car3 name:" + car3.name);
+
+        Car car4 = null;
+        System.out.println(car4.name);
+
+
+
+
     }
+
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double traveledDistance;
+    boolean running;
+    int doorCount;
+
+
+}
